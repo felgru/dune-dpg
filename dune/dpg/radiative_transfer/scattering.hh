0954302be9de97d182b39c1d7cedde6c5ef03eed
Felix Gruber <gruber@igpm.rwth-aachen.de>
2015-05-12 12:29:28 +0200

    Add a simple radiative transfer example

---
+++ dune/dpg/radiative_transfer/scattering.hh
@@ -0,0 +1,254 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_DPG_RADIATIVE_TRANSFER_SCATTERING_HH
+#define DUNE_DPG_RADIATIVE_TRANSFER_SCATTERING_HH
+
+#include <tuple>
+#include <functional>
+#include <memory>
+#include <type_traits>
+
+#include <dune/dpg/assemble_types.hh>
+#include <dune/dpg/assemble_helper.hh>
+
+#include <boost/fusion/algorithm/iteration/for_each.hpp>
+
+#include <boost/math/constants/constants.hpp>
+
+namespace Dune {
+
+/**
+ * \brief This constructs the right hand side vector of a DPG system.
+ *
+ * \tparam TestSpaces      tuple of test spaces
+ * \tparam SolutionSpaces  tuple of solution spaces
+ * \tparam FormulationType either SaddlepointFormulation or DPGFormulation
+ */
+template<class TestSpaces,
+         class SolutionSpaces,
+         class FormulationType>
+class ScatteringAssembler
+{
+public:
+  ScatteringAssembler () = delete;
+  /**
+   * \brief constructor for ScatteringAssembler
+   *
+   * \note For your convenience, use make_ScatteringAssembler() instead.
+   */
+  constexpr ScatteringAssembler (TestSpaces testSpaces,
+                                 SolutionSpaces solutionSpaces)
+             : testSpaces(testSpaces),
+               solutionSpaces(solutionSpaces)
+  { };
+
+  /**
+   * \brief Assemble the vector corresponding to the scattering integral
+   * for a given set of discrete ordinate solutions.
+   *
+   * \todo: For now, the scattering kernel is assumed to be constant 1.
+   *
+   * \param[out] rhs  the scattering vector
+   * \param[in]  i    index of the scattering direction
+   * \param[in]  x    the vectors of the solutions of the previous iteration
+   */
+  template<size_t solutionSpaceIndex>
+  void assembleScattering
+         (BlockVector<FieldVector<double,1> >& rhs,
+          int i,
+          const std::vector<BlockVector<FieldVector<double,1> >>& x);
+
+private:
+  TestSpaces     testSpaces;
+  SolutionSpaces solutionSpaces;
+};
+
+/**
+ * \brief Creates an ScatteringAssembler,
+ *        deducing the target type from the types of arguments.
+ *
+ * \param testSpaces     a tuple of test spaces
+ * \param testSpaces     a tuple of solution spaces
+ * \tparam FormulationType either SaddlepointFormulation or DPGFormulation
+ */
+template<class TestSpaces,
+         class SolutionSpaces,
+         class FormulationType>
+auto make_ScatteringAssembler(TestSpaces testSpaces,
+                              SolutionSpaces solutionSpaces,
+                              FormulationType)
+     -> ScatteringAssembler<TestSpaces, SolutionSpaces, FormulationType>
+{
+  return ScatteringAssembler<TestSpaces, SolutionSpaces,
+                             FormulationType>
+                            (testSpaces, solutionSpaces);
+}
+
+template<class TestSpaces,
+         class SolutionSpaces,
+         class FormulationType>
+template<size_t solutionSpaceIndex>
+void ScatteringAssembler<TestSpaces, SolutionSpaces, FormulationType>::
+assembleScattering(BlockVector<FieldVector<double,1> >& rhs,
+                   int i,
+                   const std::vector<BlockVector<FieldVector<double,1> >>& x)
+{
+  using namespace boost::fusion;
+  using namespace Dune::detail;
+
+  constexpr bool isSaddlepoint =
+        std::is_same<
+             typename std::decay<FormulationType>::type
+           , SaddlepointFormulation
+        >::value;
+
+  const int numS = x.size();
+
+  // Get the grid view from the finite element basis
+  typedef typename std::tuple_element<0,TestSpaces>::type::GridView GridView;
+  GridView gridView = std::get<0>(testSpaces).gridView();
+
+  auto testBasisIndexSet     = as_vector(transform(testSpaces,
+                                                   getIndexSet()));
+  auto solutionBasisIndexSet = as_vector(transform(solutionSpaces,
+                                                   getIndexSet()));
+
+  /* set up global offsets */
+  size_t globalTestSpaceOffsets[std::tuple_size<TestSpaces>::value];
+  size_t globalSolutionSpaceOffsets[std::tuple_size<SolutionSpaces>::value];
+  size_t globalTotalTestSize = 0;
+
+  if(isSaddlepoint) {
+    fold(zip(globalTestSpaceOffsets, testBasisIndexSet),
+         0, globalOffsetHelper());
+    globalTotalTestSize =
+        globalTestSpaceOffsets[std::tuple_size<TestSpaces>::value-1]
+        + at_c<std::tuple_size<TestSpaces>::value-1>(testBasisIndexSet).size();
+  } else { /* DPG formulation */
+    for(size_t i=0; i<std::tuple_size<TestSpaces>::value; ++i)
+    {
+      globalTestSpaceOffsets[i] = 0;
+    }
+  }
+
+  fold(zip(globalSolutionSpaceOffsets, solutionBasisIndexSet),
+       isSaddlepoint?globalTotalTestSize:0, globalOffsetHelper());
+
+  size_t globalTotalSolutionSize =
+      globalSolutionSpaceOffsets[std::tuple_size<SolutionSpaces>::value-1]
+      + at_c<std::tuple_size<SolutionSpaces>::value-1>
+            (solutionBasisIndexSet).size()
+      - globalSolutionSpaceOffsets[0];
+
+  double globalSolutionSpaceOffset =
+      globalSolutionSpaceOffsets[solutionSpaceIndex];
+
+  // set rhs to the right size
+  rhs.resize(globalTotalTestSize
+             + (isSaddlepoint?globalTotalSolutionSize:0));
+  rhs = 0;
+
+  // Views on the FE bases on a single element
+  auto testLocalView     = as_vector(transform(testSpaces, getLocalView()));
+  auto solutionLocalView = as_vector(transform(solutionSpaces, getLocalView()));
+
+  auto testLocalIndexSet     = as_vector(transform(testBasisIndexSet,
+                                                   getLocalIndexSet()));
+  auto solutionLocalIndexSet = as_vector(transform(solutionBasisIndexSet,
+                                                   getLocalIndexSet()));
+
+  // A loop over all elements of the grid
+  for(const auto& e : elements(gridView)) {
+
+    // Bind the local FE basis view to the current element
+    /* TODO: only bind the space we use later */
+    for_each(solutionLocalView, applyBind<decltype(e)>(e));
+    for_each(testLocalView, applyBind<decltype(e)>(e));
+
+    for_each(zip(solutionLocalIndexSet, solutionLocalView),
+             make_fused_procedure(bindLocalIndexSet()));
+    for_each(zip(testLocalIndexSet, testLocalView),
+             make_fused_procedure(bindLocalIndexSet()));
+
+    // Now get the local contribution to the right-hand side vector
+
+    // Get the grid element from the local FE basis view
+    typedef typename std::remove_reference<decltype(e)>::type Element;
+
+    const int dim = Element::dimension;
+
+    // Get set of shape functions for this element
+    const auto& localFiniteElementTest =
+        at_c<0>(testLocalView)->tree().finiteElement();
+    const auto& localFiniteElementSolution =
+        at_c<solutionSpaceIndex>(solutionLocalView)->tree().finiteElement();
+
+    BlockVector<FieldVector<double,1> >
+        localScattering(localFiniteElementTest.localBasis().size());
+
+    // Set all entries to zero
+    localScattering = 0;
+
+    // A quadrature rule
+    int order = dim*localFiniteElementTest.localBasis().order(); //TODO!!!!!!
+    const QuadratureRule<double, dim>& quad =
+        QuadratureRules<double, dim>::rule(e.type(), order);
+
+
+    // Loop over all quadrature points
+    for ( size_t pt=0; pt < quad.size(); pt++ ) {
+
+      // Position of the current quadrature point in the reference element
+      const FieldVector<double,dim>& quadPos = quad[pt].position();
+
+      // The multiplicative factor in the integral transformation formula
+      const double integrationElement = e.geometry().integrationElement(quadPos);
+
+      /* TODO: evaluate u_s_i(quadPos) */
+      double uValue = 0;
+      {
+        // Evaluate all shape function values at this point
+        std::vector<FieldVector<double,1> > shapeFunctionValues;
+        localFiniteElementSolution.localBasis().
+            evaluateFunction(quadPos, shapeFunctionValues);
+        for (size_t j=0; j<shapeFunctionValues.size(); j++)
+        {
+          /* TODO: Did we define solutionLocalIndexSet and
+           *       globalSolutionSpaceOffset? */
+          auto row = at_c<solutionSpaceIndex>(solutionLocalIndexSet)->index(j)[0]
+                     + globalSolutionSpaceOffset;
+          uValue += x[i][row] * shapeFunctionValues[j];
+        }
+      }
+
+      // Evaluate all shape function values at this point
+      std::vector<FieldVector<double,1> > shapeFunctionValues;
+      localFiniteElementTest.localBasis().evaluateFunction(quadPos, shapeFunctionValues);
+
+      // Actually compute the vector entries
+      const double factor = 2 * boost::math::constants::pi<double>()/numS
+                            * uValue * quad[pt].weight() * integrationElement;
+      for (size_t i=0, i_max=localScattering.size(); i<i_max; i++)
+        localScattering[i] += factor * shapeFunctionValues[i];
+
+    }
+
+    auto rhsCopier = localToGlobalRHSCopier
+                     <typename remove_reference<decltype(rhs)>::type>(rhs);
+    rhsCopier(localScattering,
+              at_c<0>(testLocalIndexSet),
+              globalTestSpaceOffsets[0]
+             );
+
+  }
+
+  /* free memory handled by raw pointers */
+  for_each(solutionLocalIndexSet, default_deleter());
+  for_each(testLocalIndexSet,     default_deleter());
+  for_each(solutionLocalView,     default_deleter());
+  for_each(testLocalView,         default_deleter());
+}
+
+} // end namespace Dune
+
+#endif // DUNE_DPG_RADIATIVE_TRANSFER_SCATTERING_HH
---
+++ src/dune_dpg_rad_trans.cc
@@ -0,0 +1,331 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+#include <iostream>
+#include <cstdlib> // for std::abort()
+
+#include <vector>
+
+#include <dune/common/exceptions.hh> // We use exceptions
+#include <dune/common/function.hh>
+#include <dune/common/bitsetvector.hh>
+
+#include <dune/geometry/quadraturerules.hh>
+
+#include <dune/grid/common/intersection.hh> //TODO necessary?
+#include <dune/grid/io/file/vtk/subsamplingvtkwriter.hh>
+#include <dune/grid/uggrid.hh>
+#include <dune/grid/utility/structuredgridfactory.hh>
+#include <dune/grid/io/file/vtk.hh>
+#include <dune/grid/io/file/gmshreader.hh>
+
+
+
+#include <dune/istl/matrix.hh>
+#include <dune/istl/bcrsmatrix.hh>
+#include <dune/istl/matrixindexset.hh>
+#include <dune/istl/preconditioners.hh>
+#include <dune/istl/solvers.hh>
+#include <dune/istl/io.hh>
+#include <dune/istl/umfpack.hh>
+
+
+#include <dune/functions/functionspacebases/pqknodalbasis.hh>
+#include <dune/functions/functionspacebases/pqktracenodalbasis.hh>
+#include <dune/functions/functionspacebases/optimaltestbasis.hh>
+#include <dune/functions/functionspacebases/lagrangedgbasis.hh>
+
+#include <dune/functions/functionspacebases/interpolate.hh>
+#include <dune/functions/gridfunctions/discretescalarglobalbasisfunction.hh>
+#include <dune/functions/gridfunctions/gridviewfunction.hh>
+
+#include <dune/dpg/system_assembler.hh>
+#include <dune/dpg/boundarytools.hh>
+#include <dune/dpg/radiative_transfer/scattering.hh>
+
+#include <boost/math/constants/constants.hpp>
+
+
+using namespace Dune;
+
+
+
+int main(int argc, char** argv)
+{
+  try{
+  ///////////////////////////////////
+  //   Generate the grid
+  ///////////////////////////////////
+
+  const int dim = 2;
+  typedef UGGrid<dim> GridType;
+
+  FieldVector<double,dim> lower = {0,0};
+  FieldVector<double,dim> upper = {1,1};
+  array<unsigned int,dim> elements = {10,10};
+
+  //shared_ptr<GridType> grid = StructuredGridFactory<GridType>::createCubeGrid(lower, upper, elements);
+
+  shared_ptr<GridType> grid = StructuredGridFactory<GridType>::createSimplexGrid(lower, upper, elements);
+
+  //shared_ptr<GridType> grid = shared_ptr<GridType>(GmshReader<GridType>::read("irregular-square.msh"));
+
+  typedef GridType::LeafGridView GridView;
+  GridView gridView = grid->leafGridView();
+
+  /////////////////////////////////////////////////////////
+  //   Choose a finite element space
+  /////////////////////////////////////////////////////////
+
+  typedef Functions::LagrangeDGBasis<GridView, 1> FEBasisInterior; // u
+  FEBasisInterior feBasisInterior(gridView);
+
+  typedef Functions::PQKTraceNodalBasis<GridView, 2> FEBasisTrace; // u^
+  FEBasisTrace feBasisTrace(gridView);
+
+  auto solutionSpaces = std::make_tuple(FEBasisInterior(gridView), FEBasisTrace(gridView));
+
+  typedef Functions::LagrangeDGBasis<GridView, 4> FEBasisTest;     // v enriched
+  auto testSpaces = std::make_tuple(FEBasisTest(gridView));
+
+  typedef decltype(testSpaces) TestSpaces;
+  typedef decltype(solutionSpaces) SolutionSpaces;
+
+  typedef decltype(make_BilinearForm(testSpaces, solutionSpaces,
+            make_tuple(
+              make_IntegralTerm<0,0,IntegrationType::valueValue,
+                                    DomainOfIntegration::interior>(0.),
+              make_IntegralTerm<0,0,IntegrationType::gradValue,
+                                    DomainOfIntegration::interior>(-1.,
+                                       FieldVector<double, dim>{1.,1.}),
+              make_IntegralTerm<0,1,IntegrationType::normalVector,
+                                    DomainOfIntegration::face>(1.,
+                                       FieldVector<double, dim>{1.,1.}))))
+          BilinearForm;
+  typedef decltype(make_InnerProduct(testSpaces,
+            make_tuple(
+              make_IntegralTerm<0,0,IntegrationType::valueValue,
+                                    DomainOfIntegration::interior>(1.),
+              make_IntegralTerm<0,0,IntegrationType::gradGrad,
+                                    DomainOfIntegration::interior>(1.,
+                                       FieldVector<double, dim>{1.,1.}))))
+          InnerProduct;
+
+  typedef Functions::OptimalTestBasis<BilinearForm, InnerProduct>
+      FEBasisOptimalTest;              // v
+
+  typedef decltype(make_SystemAssembler(
+#if 0
+              std::declval<std::tuple<FEBasisOptimalTest>>(), solutionSpaces,
+#else
+              testSpaces, solutionSpaces,
+#endif
+              std::declval<BilinearForm>(),
+              std::declval<InnerProduct>(), SaddlepointFormulation()))
+          SystemAssembler_t;
+
+  if(argc != 2) {
+      std::cerr << "Usage: " << argv[0] << " <# of ordinates>"
+                << std::endl;
+      std::abort();
+  }
+  int numS = atoi(argv[1]);
+
+  std::vector<SystemAssembler_t> systemAssemblers;
+  systemAssemblers.reserve(numS);
+
+  std::vector<ScatteringAssembler<TestSpaces, SolutionSpaces,
+                                  SaddlepointFormulation>
+             > scatteringAssemblers;
+  scatteringAssemblers.reserve(numS);
+
+  /* TODO: not thread-safe, each thread will need its own
+   *       FEBasisOptimalTest */
+#if 0
+  FEBasisOptimalTest feBasisTest(bilinearForm, innerProduct);
+  auto optimalTestSpaces = make_tuple(feBasisTest);
+#endif
+
+  for(int i = 0; i < numS; ++i)
+  {
+    using namespace boost::math::constants;
+    FieldVector<double, dim> s = {cos(2*pi<double>()*i/numS),
+                                  sin(2*pi<double>()*i/numS)};
+    auto bilinearForm = make_BilinearForm(testSpaces, solutionSpaces,
+          make_tuple(
+              make_IntegralTerm<0,0,IntegrationType::valueValue,
+                                    DomainOfIntegration::interior>(2.),
+              make_IntegralTerm<0,0,IntegrationType::gradValue,
+                                    DomainOfIntegration::interior>(-1., s),
+              make_IntegralTerm<0,1,IntegrationType::normalVector,
+                                    DomainOfIntegration::face>(1., s)));
+    auto innerProduct = make_InnerProduct(testSpaces,
+          make_tuple(
+              make_IntegralTerm<0,0,IntegrationType::valueValue,
+                                    DomainOfIntegration::interior>(1.),
+              make_IntegralTerm<0,0,IntegrationType::gradGrad,
+                                    DomainOfIntegration::interior>(1., s)));
+
+    systemAssemblers.emplace_back(
+        make_SystemAssembler(testSpaces, solutionSpaces,
+                             bilinearForm, innerProduct,
+                             SaddlepointFormulation()));
+    scatteringAssemblers.emplace_back(
+        make_ScatteringAssembler(testSpaces,
+                                 solutionSpaces,
+                                 SaddlepointFormulation()));
+  }
+
+  /////////////////////////////////////////////////////////
+  //   Stiffness matrix and right hand side vector
+  /////////////////////////////////////////////////////////
+
+
+  typedef BlockVector<FieldVector<double,1> > VectorType;
+  typedef BCRSMatrix<FieldMatrix<double,1,1> > MatrixType;
+
+  std::vector<VectorType> rhs(numS);
+  std::vector<MatrixType> stiffnessMatrix(numS);
+
+  // Determine Dirichlet dofs for u^ (inflow boundary)
+  std::vector<std::vector<bool>> dirichletNodesInflow(numS);
+  for(int i = 0; i < numS; ++i)
+  {
+    using namespace boost::math::constants;
+    FieldVector<double, dim> s = {cos(2*pi<double>()*i/numS),
+                                  sin(2*pi<double>()*i/numS)};
+    BoundaryTools boundaryTools = BoundaryTools();
+    boundaryTools.boundaryTreatmentInflow(std::get<1>(solutionSpaces),
+                                          dirichletNodesInflow[i],
+                                          s);
+  }
+
+  /////////////////////////////////////////////////
+  //   Choose an initial iterate
+  /////////////////////////////////////////////////
+  std::vector<VectorType> x;
+  x.reserve(numS);
+  for(int i = 0; i < numS; ++i)
+  {
+    x.emplace_back(std::get<0>(testSpaces).indexSet().size()
+                   +feBasisTrace.indexSet().size()
+                   +feBasisInterior.indexSet().size());
+    x[i] = 0;
+  }
+
+  const int N = 5;
+  for(int n = 0; n < N; ++n)
+  {
+
+    /////////////////////////////////////////////////////////
+    //  Assemble the systems
+    /////////////////////////////////////////////////////////
+    using Domain = GridType::template Codim<0>::Geometry::GlobalCoordinate;
+    auto f = std::make_tuple([] (const Domain& x) { return 1.;});
+    for(int i = 0; i < numS; ++i)
+    {
+      systemAssemblers[i].assembleSystem(stiffnessMatrix[i], rhs[i], f);
+      VectorType scattering;
+      scatteringAssemblers[i].assembleScattering<0>(scattering, i, x);
+      rhs[i] += scattering;
+      systemAssemblers[i].applyDirichletBoundarySolution<1>
+          (stiffnessMatrix[i],
+           rhs[i],
+           dirichletNodesInflow[i],
+           0.);
+    }
+
+    // std::ofstream of("stiffnessNew.dat");
+    // printmatrix(of, stiffnessMatrix[0], "stiffnessNew", "--");
+
+    ////////////////////////////
+    //   Compute solution
+    ////////////////////////////
+
+    std::cout <<"rhs size = "<< rhs[0].size()
+              <<" matrix size = " << stiffnessMatrix[0].N()
+                         << " x " << stiffnessMatrix[0].M()
+              <<" solution size = "<< x[0].size() <<std::endl;
+
+
+#if 1
+    for(int i = 0; i < numS; ++i)
+    {
+      UMFPack<MatrixType> umfPack(stiffnessMatrix[i], 2);
+      InverseOperatorResult statistics;
+      umfPack.apply(x[i], rhs[i], statistics);
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////
+    //  Make a discrete function from the FE basis and the coefficient vector
+    //////////////////////////////////////////////////////////////////////////
+
+    for(int i = 0; i < numS; ++i)
+    {
+      VectorType u(feBasisInterior.indexSet().size());
+      u=0;
+      size_t u_offset = std::get<0>(testSpaces).indexSet().size();
+      for (unsigned int j=0; j<feBasisInterior.indexSet().size(); j++)
+      {
+        u[j] = x[i][u_offset+j];
+      }
+
+      Dune::Functions::DiscreteScalarGlobalBasisFunction
+          <decltype(feBasisInterior),decltype(u)>
+          uFunction(feBasisInterior,u);
+      auto localUFunction = localFunction(uFunction);
+
+      ////////////////////////////////////////////////////////////////////////
+      //  Write result to VTK file
+      //  We need to subsample, because VTK cannot natively display
+      //  real second-order functions
+      ////////////////////////////////////////////////////////////////////////
+      SubsamplingVTKWriter<GridView> vtkWriter(gridView,2);
+      vtkWriter.addVertexData(localUFunction,
+                      VTK::FieldInfo("u", VTK::FieldInfo::Type::scalar, 1));
+      std::string name = std::string("solution_rad_trans_n")
+                       + std::to_string(n)
+                       + std::string("_s")
+                       + std::to_string(i);
+      vtkWriter.write(name);
+
+
+#if 0
+      VectorType q(feBasisTrace.indexSet().size());
+      q=0;
+      for (unsigned int j=0; j<feBasisTrace.indexSet().size(); j++)
+      {
+        q[j] = dirichletNodesInflow[i][j] ? 1. : 0.;
+      }
+
+      Dune::Functions::DiscreteScalarGlobalBasisFunction
+          <decltype(feBasisTrace),decltype(q)>
+          qFunction(feBasisTrace,q);
+      auto localQFunction = localFunction(qFunction);
+
+      ////////////////////////////////////////////////////////////////////////
+      //  Write result to VTK file
+      //  We need to subsample, because VTK cannot natively display
+      //  real second-order functions
+      ////////////////////////////////////////////////////////////////////////
+      SubsamplingVTKWriter<GridView> qvtkWriter(gridView,0);
+      qvtkWriter.addVertexData(localQFunction,
+                      VTK::FieldInfo("q", VTK::FieldInfo::Type::scalar, 1));
+      std::string qname = std::string("solution_rad_trans_q_s")
+                       + std::to_string(i) + std::string("_");
+      qvtkWriter.write(qname);
+#endif
+    }
+#endif
+  }
+
+  return 0;
+  }
+  catch (Exception &e){
+    std::cerr << "Dune reported error: " << e << std::endl;
+  }
+  catch (...){
+    std::cerr << "Unknown exception thrown!" << std::endl;
+  }
+}
