18882fb027ef25ac2b861052f54abc08ffbb7aff
Felix Gruber <gruber@igpm.rwth-aachen.de>
2016-01-25 14:06:12 +0100

    [cleanup] formatting of dune_dpg_error
    # Conflicts:
    #	src/dune_dpg_error.cc

--- src/dune_dpg_error.cc
+++ src/dune_dpg_error.cc
@@ -54,8 +54,8 @@ double fieldRHS(const Dune::FieldVector<double, 2>& x) {
 
   return beta0*c0*std::exp(c0*x[0])*(std::exp(c1*x[1])-std::exp(1.))
         +beta1*c1*std::exp(c1*x[1])*(std::exp(c0*x[0])-std::exp(1.))
-        +(std::exp(c0*x[0])-std::exp(1.))*(std::exp(c1*x[1])-std::exp(1.)) ;
-  //return 3*std::exp(x[0]+x[1])-2*(std::exp(x[0])+std::exp(x[1]))+1  ;
+        +(std::exp(c0*x[0])-std::exp(1.))*(std::exp(c1*x[1])-std::exp(1.));
+  //return 3*std::exp(x[0]+x[1])-2*(std::exp(x[0])+std::exp(x[1]))+1;
 }
 
 // The exact transport solution
@@ -64,7 +64,7 @@ double fieldExact(const Dune::FieldVector<double, 2>& x) {
   double c0 = 1.;
   double c1 = 1.;
 
-  return (std::exp(c0*x[0])-std::exp(1.))*(std::exp(c1*x[1])-std::exp(1.)) ;
+  return (std::exp(c0*x[0])-std::exp(1.))*(std::exp(c1*x[1])-std::exp(1.));
 }
 
 int main()
@@ -75,23 +75,23 @@ int main()
   //   Generate the grid
   ///////////////////////////////////
 
-   const int dim = 2;
+  const int dim = 2;
 
-   typedef UGGrid<dim> GridType;
-   FieldVector<double,dim> lower = {0,0};
-   FieldVector<double,dim> upper = {1,1};
-   array<unsigned int,dim> elements = {10,10};
+  typedef UGGrid<dim> GridType;
+  FieldVector<double,dim> lower = {0,0};
+  FieldVector<double,dim> upper = {1,1};
+  array<unsigned int,dim> elements = {10,10};
 
-   // Square mesh
-   //shared_ptr<GridType> grid = StructuredGridFactory<GridType>::createCubeGrid(lower, upper, elements);
-   // Triangular mesh
-   shared_ptr<GridType> grid  =  StructuredGridFactory<GridType>::createSimplexGrid(lower, upper, elements);
-   // Read mesh from an input file
-   // shared_ptr<GridType> grid = shared_ptr<GridType>(GmshReader<GridType>::read("irregular-square.msh")); // for an irregular mesh square
-   // shared_ptr<GridType> grid = shared_ptr<GridType>(GmshReader<GridType>::read("circle.msh")); // for an irregular mesh square
+  // Square mesh
+  //shared_ptr<GridType> grid = StructuredGridFactory<GridType>::createCubeGrid(lower, upper, elements);
+  // Triangular mesh
+  shared_ptr<GridType> grid  =  StructuredGridFactory<GridType>::createSimplexGrid(lower, upper, elements);
+  // Read mesh from an input file
+  // shared_ptr<GridType> grid = shared_ptr<GridType>(GmshReader<GridType>::read("irregular-square.msh")); // for an irregular mesh square
+  // shared_ptr<GridType> grid = shared_ptr<GridType>(GmshReader<GridType>::read("circle.msh")); // for an irregular mesh square
 
-    typedef GridType::LeafGridView GridView;
-    GridView gridView = grid->leafGridView();
+  typedef GridType::LeafGridView GridView;
+  GridView gridView = grid->leafGridView();
 
 
   /////////////////////////////////////////////////////////
@@ -216,13 +216,13 @@ int main()
   u=0;
   theta=0;
 
-  //We extract the solution vector u
+  // We extract the solution vector u
   for (size_t i=0; i<nInner; i++)
   {
     u[i] = x[i];
   }
 
-  //We extract the solution vector theta of the faces
+  // We extract the solution vector theta of the faces
   for (size_t i=0; i<nFace; i++)
   {
     theta[i] = x[nInner+i];
@@ -254,23 +254,23 @@ int main()
   // Error tolerance to do h-refinement
   double adaptivityTol = 0.001;
 
-  //We build an object of type ErrorTools to study errors, residuals and do hp-adaptivity
+  // We build an object of type ErrorTools to study errors, residuals and do hp-adaptivity
   ErrorTools errorTools = ErrorTools(adaptivityTol);
 
-  //We compute the L2 error between the exact and the fem solutions
-  double err = errorTools.computeL2error(innerSpace,u,uExact) ;
-  std::cout << "'Exact' error u: || u - u_fem ||_L2 = " << err << std::endl ;
+  // We compute the L2 error between the exact and the fem solutions
+  double err = errorTools.computeL2error(innerSpace,u,uExact);
+  std::cout << "'Exact' error u: || u - u_fem ||_L2 = " << err << std::endl;
 
-  //// todo: h-refinement
+  //// TODO: h-refinement
   //errorTools->hRefinement(grid);
-  //// todo: p-refinement
+  //// TODO: p-refinement
 
   // A posteriori error
-      //We compute the rhs in the form given by the projection approach
+  // We compute the rhs in the form given by the projection approach
   rhsAssembler.assembleRhs(rhs, rightHandSide);
-      //It is necessary to provide rhs in the above form to call this aPosterioriError method
-  double aposterioriErr = errorTools.aPosterioriError(bilinearForm,innerProduct,u,theta,rhs) ;
-  std::cout << "A posteriori error: || (u,trace u) - (u_fem,theta) || = " << aposterioriErr << std::endl ;
+  // It is necessary to provide rhs in the above form to call this aPosterioriError method
+  double aposterioriErr = errorTools.aPosterioriError(bilinearForm,innerProduct,u,theta,rhs);
+  std::cout << "A posteriori error: || (u,trace u) - (u_fem,theta) || = " << aposterioriErr << std::endl;
 
   //////////////////////////////////////////////////////////////////////////////////////////////
   //  Write result to VTK file
@@ -280,11 +280,11 @@ int main()
   vtkWriter.addVertexData(localUFunction, VTK::FieldInfo("u", VTK::FieldInfo::Type::scalar, 1));
   vtkWriter.write("solution_transport");
 
- SubsamplingVTKWriter<GridView> vtkWriter1(gridView,2);
- vtkWriter1.addVertexData(localThetaFunction, VTK::FieldInfo("theta",VTK::FieldInfo::Type::scalar, 1));
- vtkWriter1.write("solution_trace");
+  SubsamplingVTKWriter<GridView> vtkWriter1(gridView,2);
+  vtkWriter1.addVertexData(localThetaFunction, VTK::FieldInfo("theta",VTK::FieldInfo::Type::scalar, 1));
+  vtkWriter1.write("solution_trace");
 
-    return 0;
+  return 0;
   }
   catch (Exception &e){
     std::cerr << "Dune reported error: " << e << std::endl;
