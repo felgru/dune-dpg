40ef9ac7769939f8ae538bc810459cc5a0dff59c
Felix Gruber <gruber@igpm.rwth-aachen.de>
2016-02-03 20:17:35 +0100

    computation of convergence error
    Add scripts to generate and plot convergence data for L_2 error and
    a posteriori error estimator.

---
+++ scripts/run_convergence_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+N="10 20 40 60 80 100 120 140 160 200 250"
+for n in $N
+do
+  for CONVERGENCE_TEST in src/convergence_test_ls*
+  do
+    echo ${CONVERGENCE_TEST} $n
+    eval ${CONVERGENCE_TEST} $n
+  done
+done
