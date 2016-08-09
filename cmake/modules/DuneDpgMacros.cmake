e7682b62ac3eeaf0998562939d6d798fa91651b3
Felix Gruber <gruber@igpm.rwth-aachen.de>
2016-08-09 17:24:02 +0200

    [CMake] set BoostFusion_FOUND
    This prevents BoostFusion from appearing in the list of unfound optinal
    packages.
    
    We also mark Boost Fusion as a required package, as dune-dpg will not
    work without it.

--- cmake/modules/DuneDpgMacros.cmake
+++ cmake/modules/DuneDpgMacros.cmake
@@ -1,5 +1,5 @@
 # File for module specific CMake tests.
 include(DuneBoost)
-find_package(BoostFusion)
+find_package(BoostFusion REQUIRED)
 find_package(SuiteSparse COMPONENTS UMFPACK)
 include(AddSuiteSparseFlags)
