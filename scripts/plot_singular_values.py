f9b0bf9c0657d9b9d7dc2b56f69a9f516d48ac01
Felix Gruber <gruber@igpm.rwth-aachen.de>
2016-09-23 18:15:26 +0200

    specify file name of SVD plots
    Now we can give the file name as a command line argument.

--- scripts/plot_singular_values.py
+++ scripts/plot_singular_values.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 import fileinput
 import matplotlib.pyplot as plt
+import sys
 
+if len(sys.argv) < 2:
+    outname = 'singular_values.pdf'
+else:
+    outname = sys.argv[1]
 singularValues = list()
-for line in fileinput.input():
+for line in fileinput.input(sys.argv[2:]):
     singularValues.append(float(line))
 plt.plot(singularValues)
+plt.xlim(0, len(singularValues)-1)
 plt.yscale('log')
-plt.savefig('singular_values.pdf')
+plt.savefig(outname)
