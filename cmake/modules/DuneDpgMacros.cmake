96fe44a1f5076dd1a0176a37fa6bdc375d0f2bea
Angela <klewinghaus@igpm.rwth-aachen.de>
2016-10-16 18:55:45 +0200

    Enable also BoostFusion 1.58

--- cmake/modules/DuneDpgMacros.cmake
+++ cmake/modules/DuneDpgMacros.cmake
@@ -1,5 +1,5 @@
 # File for module specific CMake tests.
 include(DuneBoost)
-find_package(BoostFusion 1.59 REQUIRED)
+find_package(BoostFusion 1.58 REQUIRED)
 find_package(SuiteSparse COMPONENTS UMFPACK)
 include(AddSuiteSparseFlags)
