848bdba497678331eceda0cf7771192cf3e374c7
Felix Gruber <gruber@igpm.rwth-aachen.de>
2016-11-30 18:00:30 +0100

    [cleanup] assemble_types.hh: remove unused definitions

--- dune/dpg/assemble_types.hh
+++ dune/dpg/assemble_types.hh
@@ -21,21 +21,12 @@ enum class IntegrationType {
 enum class LinearIntegrationType {
     valueFunction,           //!< v f where f is an explicit function known at every point of the domain
     gradFunction,            //!< ∇v · f where f is an explicit function known at every point of the domain
-    valueValueScattering,    //!< v Scattering where Scattering is $\int_{sphere} collision_kernel density function$
-                             //!<              and density function is a FE polynomial
-    valueFunctionScattering, //!< v Scattering where Scattering is $\int_{sphere} collision_kernel density function$
-                             //!<              and density function has an explicit formula
     normalVectorFunction     //!< v f n · β, where n is the outer unit normal vector and f is an explicit function known at every point of the domain
 };
 
 enum class EvaluationType {
     value,   //!< v
     grad,    //!< ∇v
-};
-
-enum class SpaceType {
-    test,     //!< a test space
-    solution  //!< a solution space
 };
 
 //! The saddlepoint formulation of a DPG system.
