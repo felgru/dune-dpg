ea9107ae141f48ee6b2d03fe300c0182c69cd940
Felix Gruber <gruber@igpm.rwth-aachen.de>
2017-02-15 18:26:36 +0100

    ConstrainedDiscreteGlobalBasisFunction

---
+++ dune/dpg/functions/constraineddiscreteglobalbasisfunction.hh
@@ -0,0 +1,239 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_FUNCTIONS_DPG_CONSTRAINEDDISCRETEGLOBALBASISFUNCTIONS_HH
+#define DUNE_FUNCTIONS_DPG_CONSTRAINEDDISCRETEGLOBALBASISFUNCTIONS_HH
+
+#include <memory>
+
+#include <dune/common/shared_ptr.hh>
+
+#include <dune/functions/functionspacebases/defaultnodetorangemap.hh>
+#include <dune/functions/functionspacebases/flatvectorbackend.hh>
+#include <dune/functions/gridfunctions/gridviewentityset.hh>
+#include <dune/functions/gridfunctions/gridfunction.hh>
+#include <dune/functions/common/treedata.hh>
+
+namespace Dune {
+namespace Functions {
+
+
+
+template<typename B, typename V,
+         typename R = typename V::value_type>
+class ConstrainedDiscreteGlobalBasisFunction
+{
+public:
+  using Basis = B;
+  using Vector = V;
+
+  using GridView = typename Basis::GridView;
+  using EntitySet = GridViewEntitySet<GridView, 0>;
+  using Node = typename Basis::LocalView::Tree;
+
+  using Domain = typename EntitySet::GlobalCoordinate;
+  using Range = R;
+
+  using LocalDomain = typename EntitySet::LocalCoordinate;
+  using Element = typename EntitySet::Element;
+
+  using Traits = Imp::GridFunctionTraits<Range(Domain), EntitySet,
+                                         DefaultDerivativeTraits, 16>;
+
+  class LocalFunction
+  {
+    using LocalBasisView = typename Basis::LocalView;
+    using LocalIndexSet = typename Basis::LocalIndexSet;
+    using size_type = typename Basis::size_type;
+
+    using LocalBasisRange = typename
+        Node::FiniteElement::Traits::LocalBasisType::Traits::RangeType;
+
+    using NodeData = std::vector<LocalBasisRange>;
+
+  public:
+
+    using GlobalFunction = ConstrainedDiscreteGlobalBasisFunction;
+    using Domain = LocalDomain;
+    using Range = GlobalFunction::Range;
+    using Element = GlobalFunction::Element;
+
+    LocalFunction(const ConstrainedDiscreteGlobalBasisFunction& globalFunction)
+      : globalFunction_(globalFunction)
+      , localBasisView_(globalFunction.basis().localView())
+      , localIndexSet_(globalFunction.basis().localIndexSet())
+      , node_(localBasisView_.tree())
+    {
+      shapeFunctionValues_.reserve(localBasisView_.maxSize());
+      localDoFs_.reserve(localBasisView_.maxSize());
+    }
+
+    LocalFunction(const LocalFunction& other)
+      : globalFunction_(other.globalFunction_)
+      , localBasisView_(globalFunction_.basis().localView())
+      , localIndexSet_(globalFunction_.basis().localIndexSet())
+      , node_(localBasisView_.tree())
+    {
+      shapeFunctionValues_.reserve(localBasisView_.maxSize());
+      localDoFs_.reserve(localBasisView_.maxSize());
+    }
+
+    LocalFunction operator=(const LocalFunction& other)
+    {
+      globalFunction_ = other.globalFunction_;
+      localBasisView_ = other.localBasisView_;
+      localIndexSet_ = other.localIndexSet_;
+      node_ = localBasisView_.tree();
+
+      shapeFunctionValues_.reserve(localBasisView_.maxSize());
+      localDoFs_.reserve(localBasisView_.maxSize());
+    }
+
+    /**
+     * \brief Bind LocalFunction to grid element.
+     *
+     * You must call this method before evaluation
+     * and after changes to the coefficient vector.
+     */
+    void bind(const Element& element)
+    {
+      localBasisView_.bind(element);
+      localIndexSet_.bind(localBasisView_);
+
+      localDoFs_.resize(localBasisView_.size());
+      auto globalIndex = localIndexSet_.indicesLocalGlobal().begin();
+      auto globalIndexEnd = localIndexSet_.indicesLocalGlobal().end();
+      const size_type numConstraints = localIndexSet_.constraintsSize();
+      size_type i = 0;
+      for(size_type c = 0; c < numConstraints; c++) {
+        const size_type nextConstraint = i + localIndexSet_.constraintOffset(c);
+        for (; i < nextConstraint; ++i) {
+          localDoFs_[i] = globalFunction_.dofs()[*(globalIndex++)];
+        }
+        localDoFs_[i] = 0;
+        for(auto w : localIndexSet_.constraintWeights(c)) {
+          localDoFs_[i] += w * globalFunction_.dofs()[*(globalIndex++)];
+        }
+        ++i;
+      }
+      for (; i < localDoFs_.size(); ++i)
+        localDoFs_[i] = globalFunction_.dofs()[*(globalIndex++)];
+      assert(globalIndex == globalIndexEnd);
+    }
+
+    void unbind()
+    {
+      localIndexSet_.unbind();
+      localBasisView_.unbind();
+    }
+
+    /**
+     * \brief Evaluate LocalFunction at bound element.
+     *
+     * The result of this method is undefined if you did
+     * not call bind() beforehand or changed the coefficient
+     * vector after the last call to bind(). In the latter case
+     * you have to call bind() again in order to make operator()
+     * usable.
+     */
+    Range operator()(const Domain& x) const
+    {
+      auto y = Range(0);
+
+      auto&& fe = node_.finiteElement();
+      auto&& localBasis = fe.localBasis();
+
+      shapeFunctionValues_.resize(localBasis.size());
+      localBasis.evaluateFunction(x, shapeFunctionValues_);
+
+      for(size_type i = 0; i < localBasis.size(); i++) {
+        y += localDoFs_[i] * shapeFunctionValues_[i];
+      }
+
+      return y;
+    }
+
+    const Element& localContext() const
+    {
+      return localBasisView_.element();
+    }
+
+    friend typename Traits::LocalFunctionTraits::DerivativeInterface derivative(const LocalFunction& t)
+    {
+      DUNE_THROW(NotImplemented,"not implemented");
+    }
+
+  private:
+
+    const ConstrainedDiscreteGlobalBasisFunction& globalFunction_;
+    LocalBasisView localBasisView_;
+    LocalIndexSet localIndexSet_;
+
+    mutable std::vector<typename V::value_type> shapeFunctionValues_;
+    std::vector<typename V::value_type> localDoFs_;
+    const Node& node_;
+  };
+
+  ConstrainedDiscreteGlobalBasisFunction(const Basis & basis, const V & coefficients) :
+    entitySet_(basis.gridView()),
+    basis_(basis),
+    coefficients_(coefficients)
+  {}
+
+  const Basis& basis() const
+  {
+    return basis_;
+  }
+
+  const V& dofs() const
+  {
+    return coefficients_;
+  }
+
+  // TODO: Implement this using hierarchic search
+  Range operator() (const Domain& x) const
+  {
+    DUNE_THROW(NotImplemented,"not implemented");
+  }
+
+  friend typename Traits::DerivativeInterface derivative(const ConstrainedDiscreteGlobalBasisFunction& t)
+  {
+    DUNE_THROW(NotImplemented,"not implemented");
+  }
+
+  friend LocalFunction localFunction(const ConstrainedDiscreteGlobalBasisFunction& t)
+  {
+    return LocalFunction(t);
+  }
+
+  /**
+   * \brief Get associated EntitySet
+   */
+  const EntitySet& entitySet() const
+  {
+    return entitySet_;
+  }
+
+private:
+
+  EntitySet entitySet_;
+  const Basis& basis_;
+  const V& coefficients_;
+};
+
+
+
+template<typename R, typename B, typename V>
+auto makeConstrainedDiscreteGlobalBasisFunction(const B& basis, const V& vector)
+{
+  using Basis = std::decay_t<B>;
+  using Vector = std::decay_t<V>;
+  return ConstrainedDiscreteGlobalBasisFunction<Basis, Vector, R>
+      (basis, vector);
+}
+
+
+
+} // namespace Functions
+} // namespace Dune
+
+#endif // DUNE_FUNCTIONS_DPG_CONSTRAINEDDISCRETEGLOBALBASISFUNCTIONS_HH
