26b4711166b53ed0bd1648e4ae22e2c36168fd7a
Olga Mula <olga.mula@gmail.com>
2017-08-30 10:11:31 +0200

    Create a class for Henyey kernel.

--- dune/dpg/radiative_transfer/henyey_greenstein_scattering.hh
+++ dune/dpg/radiative_transfer/henyey_greenstein_scattering.hh
@@ -10,14 +10,21 @@
 
 namespace Dune {
 
-inline auto
-HenyeyGreensteinScattering(double gamma) {
-  return [gamma](double angle) {
-    return (1-gamma*gamma)/(2 * boost::math::constants::pi<double>()
+struct HenyeyGreensteinScattering{
+  HenyeyGreensteinScattering(double gamma) : gamma(gamma) {}
+
+  double operator()(double angle) const {
+      return (1-gamma*gamma)/(2 * boost::math::constants::pi<double>()
                               * (1+gamma*gamma-2*gamma*cos(angle)));
-  };
-}
+  }
+
+  std::string info() const {
+    return "Henyey Greenstein kernel with gamma = " + std::to_string(gamma);
+  }
+
+private:
+  const double gamma;
+};
 
 }
-
 #endif // DUNE_DPG_RADIATIVE_TRANSFER_HENYEY_GREENSTEIN_SCATTERING_HH
