23003ad3d5bae25f71f9d2da54dc0aae1a9587ae
Felix Gruber <gruber@igpm.rwth-aachen.de>
2017-09-21 00:27:45 +0200

    make visualization.py executable

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -1,9 +1,3 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import scipy as sp
-# import pylab as pl
-# import math
-
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 from __future__ import (absolute_import, print_function)
