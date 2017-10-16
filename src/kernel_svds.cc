402feefa049ceca91a15d12b5d0ec70ee8c6b4b6
Felix Gruber <gruber@igpm.rwth-aachen.de>
2017-10-16 19:26:53 +0200

    fix kernel_svds.cc for wavelet kernels
    This also changes the wavelet basis from Haar to Alpert (of degree 3).
    
    The computation of SVDs for kernels in non-wavelet representation is
    currently disabled.

--- src/kernel_svds.cc
+++ src/kernel_svds.cc
@@ -1,9 +1,15 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
 #include <boost/math/constants/constants.hpp>
 
 #include <dune/common/fvector.hh>
+#include <dune/common/version.hh>
 #include <dune/dpg/radiative_transfer/kanschat_scattering.hh>
 #include <dune/dpg/radiative_transfer/henyey_greenstein_scattering.hh>
 #include <dune/dpg/radiative_transfer/waveletkernelapproximation.hh>
+#include <dune/geometry/quadraturerules.hh>
+#include <dune/geometry/type.hh>
 
 #include <Eigen/Core>
 #include <Eigen/SVD>
@@ -31,29 +37,66 @@ std::vector<double> polynomialDecay(size_t n) {
   return a;
 }
 
-template<class Direction, class Kernel>
-Eigen::MatrixXd kernelMatrix(const std::vector<Direction>& sVector, Kernel&& kernel) {
-  size_t numS = sVector.size();
+std::vector<double> angles(unsigned int wltOrder,
+                           unsigned int level)
+{
+  using namespace Dune;
+
+  // Get Gauss-Legendre quadrature in [0,1]
+  const int quadOrder = 2*wltOrder+1;
+  const size_t dim = 1;
+#if DUNE_VERSION_NEWER(DUNE_GRID,2,6)
+  const QuadratureRule<double, dim>& quad =
+    QuadratureRules<double, dim>::rule(GeometryTypes::line,
+      quadOrder, QuadratureType::GaussLegendre);
+#else
+  GeometryType type;
+  type.makeLine();
+  const QuadratureRule<double, dim>& quad =
+    QuadratureRules<double, dim>::rule(type,
+      quadOrder, QuadratureType::GaussLegendre);
+#endif
+  using namespace boost::math::constants;
+  std::vector<double> angles;
+  angles.reserve((1<<level)*quad.size());
+  for(int k = 0; k < (1<<level); ++k)
+  {
+    for (size_t pt=0, qsize=quad.size(); pt < qsize; pt++) {
+      const double angle = 2*pi<double>()*(k+quad[pt].position())
+                           / (1<<level) - pi<double>(); // Angle in [-pi,pi]
+      angles.push_back(angle);
+    }
+  }
+  return angles;
+}
+
+#if 0
+template<class Kernel>
+Eigen::MatrixXd kernelMatrix(const std::vector<double>& angles,
+                             Kernel&& kernel)
+{
+  size_t numS = angles.size();
   Eigen::MatrixXd kernelMatrix(numS, numS);
   for(size_t j = 0; j < numS; ++j) {
-    Direction s_j = sVector[j];
+    const double s_j = angles[j];
     for(size_t i = 0; i < numS; ++i) {
-      Direction s_i = sVector[i];
+      const double s_i = angles[i];
       // TODO: maybe use a higher order quadrature
-      kernelMatrix(i,j) = kernel(s_i, s_j)/numS;
+      kernelMatrix(i,j) = kernel(s_i - s_j);
     }
   }
   return kernelMatrix;
 }
+#endif
 
 void printHelp(char* name) {
-  std::cerr << "Usage: " << name << " [-e n|-p n|-a] [-w] s\n"
+  std::cerr << "Usage: " << name << " [-e n|-p n|-a] [-w] l\n"
                " -e n: exponential decay with n terms\n"
                " -p n: polynomial decay with n terms\n"
                " -g gamma: Henyey-Greenstein kernel with given gamma\n"
                " -a: kernel from Avila et al. 2011\n"
-               " -w: use Haar wavelet representation of kernel matrix\n"
-               " s: number of directions\n";
+               " -w: use Alpert wavelet representation of kernel matrix\n"
+               " l: maximal wavelet level\n";
   exit(0);
 }
 
@@ -68,7 +111,7 @@ int main(int argc, char *argv[]) {
   kernelType kt = kernelType::exponential;
   size_t kernelTerms = 50;
   double gamma = 0.;
-  size_t numS = 100;
+  size_t maxLevel = 5;
   bool wavelet = false;
 
   int opt;
@@ -87,69 +130,73 @@ int main(int argc, char *argv[]) {
   if(optind != argc-1) {
     printHelp(argv[0]);
   }
-  numS = atoi(argv[optind]);
+  if(!wavelet) {
+    std::cerr << "computation of kernel SVDs is currently only implemented"
+                 " for wavelet representation of kernels.\n";
+    exit(0);
+  }
+  maxLevel = atoi(argv[optind]);
+  const size_t wltOrder = 3;
 
   using namespace Dune;
 
-  const unsigned int dim = 2;
-  using Direction = FieldVector<double, dim>;
-
-  std::vector<Direction> sVector(numS);
-  for(size_t i = 0; i < numS; ++i)
-  {
-    using namespace boost::math::constants;
-    sVector[i] = {cos(2*pi<double>()*i/numS),
-                  sin(2*pi<double>()*i/numS)};
-  }
-
-  using namespace Dune::ScatteringKernelApproximation::HaarWavelet;
+  using namespace Dune::ScatteringKernelApproximation::AlpertWavelet;
 
   Eigen::MatrixXd m;
   switch(kt) {
     case kernelType::exponential:
       {
         auto exponentialKernel
-          = KanschatScattering<Direction>(exponentialDecay(kernelTerms));
+          = KanschatScattering(exponentialDecay(kernelTerms));
         if(wavelet) {
-          m = waveletKernelMatrix(exponentialKernel, std::ilogb(numS));
+          m = waveletKernelMatrix(exponentialKernel, wltOrder, maxLevel, 16);
+#if 0
         } else {
-          m = kernelMatrix(sVector, exponentialKernel);
+          m = kernelMatrix(angles(wltOrder, maxLevel), exponentialKernel);
+#endif
         }
       }
       break;
     case kernelType::polynomial:
       {
         auto polynomialKernel
-          = KanschatScattering<Direction>(polynomialDecay(kernelTerms));
+          = KanschatScattering(polynomialDecay(kernelTerms));
         if(wavelet) {
-          m = waveletKernelMatrix(polynomialKernel, std::ilogb(numS));
+          m = waveletKernelMatrix(polynomialKernel, wltOrder, maxLevel, 16);
+#if 0
         } else {
-          m = kernelMatrix(sVector, polynomialKernel);
+          m = kernelMatrix(angles(wltOrder, maxLevel), polynomialKernel);
+#endif
         }
       }
       break;
     case kernelType::henyey_greenstein:
       {
-        auto henyeyGreensteinKernel
-          = HenyeyGreensteinScattering<Direction>(gamma);
+        auto henyeyGreensteinKernel = HenyeyGreensteinScattering(gamma);
         if(wavelet) {
-          m = waveletKernelMatrix(henyeyGreensteinKernel, std::ilogb(numS));
+          m = waveletKernelMatrix(henyeyGreensteinKernel, wltOrder,
+                                  maxLevel, 16);
+#if 0
         } else {
-          m = kernelMatrix(sVector, henyeyGreensteinKernel);
+          m = kernelMatrix(angles(wltOrder, maxLevel), henyeyGreensteinKernel);
+#endif
         }
       }
       break;
     case kernelType::ACP2011:
       {
-        auto acpKernel = ACP2011Scattering<Direction>();
+        auto acpKernel = ACP2011Scattering();
         if(wavelet) {
-          m = waveletKernelMatrix(acpKernel, std::ilogb(numS));
+          m = waveletKernelMatrix(acpKernel, wltOrder, maxLevel, 16);
+#if 0
         } else {
-          m = kernelMatrix(sVector, acpKernel);
+          m = kernelMatrix(angles(wltOrder, maxLevel), acpKernel);
+#endif
         }
       }
   }
 
+  const size_t numS = m.rows();
   Eigen::JacobiSVD<Eigen::MatrixXd, Eigen::NoQRPreconditioner>
       kernelSVD(numS, numS, Eigen::ComputeThinU | Eigen::ComputeThinV);
   kernelSVD.compute(m);
