27cf7deb7272a86af524cbc15fecec01a80dcc3a
Felix Gruber <gruber@igpm.rwth-aachen.de>
2018-03-09 17:55:03 +0100

    plot_solution: use Bernstein basis on skeleton

--- src/plot_solution.cc
+++ src/plot_solution.cc
@@ -28,7 +28,7 @@
 #include <dune/istl/io.hh>
 #include <dune/istl/umfpack.hh>
 
-#include <dune/functions/functionspacebases/hangingnodep2nodalbasis.hh>
+#include <dune/functions/functionspacebases/hangingnodebernsteinp2basis.hh>
 #include <dune/functions/functionspacebases/lagrangedgbasis.hh>
 #include <dune/functions/functionspacebases/pqkdgrefineddgnodalbasis.hh>
 
@@ -85,7 +85,7 @@ int main(int argc, char** argv)
 
   // We use a SubGrid as it will automatically make sure that we do
   // not have more than difference 1 in the levels of neighboring
-  // elements. This is necessary since HangingNodeP2NodalBasis does
+  // elements. This is necessary since HangingNodeBernsteinP2Basis does
   // not implement higher order hanging nodes constraints.
   std::unique_ptr<Grid> grid = std::make_unique<Grid>(*hostGrid);
   {
@@ -115,7 +115,7 @@ int main(int argc, char** argv)
     using FEBasisInterior = Functions::LagrangeDGBasis<GridView, 1>;
 
     // bulk term corresponding to theta
-    using FEBasisTrace = Functions::HangingNodeP2NodalBasis<GridView>;
+    using FEBasisTrace = Functions::HangingNodeBernsteinP2Basis<GridView>;
 
     auto solutionSpaces
       = make_space_tuple<FEBasisInterior, FEBasisTrace>(gridView);
