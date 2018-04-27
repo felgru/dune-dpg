de36231a5500c0440bc93ba18f71cd270999f9fc
Felix Gruber <gruber@igpm.rwth-aachen.de>
2018-04-27 15:17:05 +0200

    add piecewise constant grid view function

--- dune/dpg/functions/gridviewfunctions.hh
+++ dune/dpg/functions/gridviewfunctions.hh
@@ -4,7 +4,9 @@
 #define DUNE_FUNCTIONS_DPG_GRIDVIEWFUNCTIONS_HH
 
 #include <type_traits>
+#include <utility>
 
+#include <dune/common/typeutilities.hh>
 #include <dune/functions/gridfunctions/gridviewentityset.hh>
 
 namespace Dune {
@@ -81,6 +83,112 @@ ConstantGridViewFunction<Constant, GridView>
 makeConstantGridViewFunction(Constant constant, const GridView&)
 {
   return {constant};
+}
+
+template<class Range, class LocalDomain, class LocalContext, class F>
+class LocalPiecewiseConstantGridViewFunction
+{
+  using Domain = LocalDomain;
+
+public:
+  LocalPiecewiseConstantGridViewFunction(Range constant) : constant(constant) {}
+
+  template<class FT,
+           disableCopyMove<LocalPiecewiseConstantGridViewFunction, FT> = 0>
+  LocalPiecewiseConstantGridViewFunction(FT&& f) :
+    f_(std::forward<FT>(f))
+  {}
+
+  Range operator() (const Domain&) const
+  {
+    return constant;
+  }
+
+  void bind(const LocalContext& localContext)
+  {
+    context = &localContext;
+    const Domain centerOfLocalContext = localContext.geometry().center();
+    constant = f_(centerOfLocalContext);
+  }
+
+  /**
+   * \brief Unbind from local context
+   */
+  void unbind()
+  {
+    context = nullptr;
+  }
+
+  /**
+   * \brief Obtain local contex this LocalFunction is bound to
+   */
+  const LocalContext& localContext() const
+  {
+    return *context;
+  }
+
+private:
+  F f_;
+  Range constant;
+  const LocalContext* context = nullptr;
+};
+
+template<class Range, class GridView, class F>
+class PiecewiseConstantGridViewFunction
+{
+  using EntitySet = GridViewEntitySet<GridView, 0>;
+  using Domain
+      = typename GridView::template Codim<0>::Geometry::GlobalCoordinate;
+  using LocalCoordinate = typename EntitySet::LocalCoordinate;
+  // using LocalSignature = typename Range(LocalCoordinate);
+
+public:
+  using Element = typename EntitySet::Element;
+
+  using LocalFunction
+    = LocalPiecewiseConstantGridViewFunction<Range, LocalCoordinate,
+                                             Element, F>;
+
+  template<class FT>
+  PiecewiseConstantGridViewFunction(FT&& f, const GridView& gridView) :
+    f_(std::forward<FT>(f)),
+    entitySet_(gridView)
+  {}
+
+  Range operator()(const Domain& x) const
+  {
+    return f_(x);
+  }
+
+  friend LocalFunction
+    localFunction(const PiecewiseConstantGridViewFunction& t)
+  {
+    return LocalFunction(t.f_);
+  }
+
+  const EntitySet& entitySet() const
+  {
+    return entitySet_;
+  }
+
+private:
+  F f_;
+  EntitySet entitySet_;
+};
+
+template<class F, class GridView>
+PiecewiseConstantGridViewFunction<
+  typename std::result_of<F(typename GridView::template Codim<0>::Geometry::GlobalCoordinate)>::type,  // Range
+  GridView,
+  typename std::decay<F>::type >                                                                      // Raw type of F (without & or &&)
+  makePiecewiseConstantGridViewFunction(F&& f, const GridView& gridView)
+{
+  using Domain = typename GridView::template Codim<0>::Geometry::GlobalCoordinate;
+  using Range = typename std::result_of<F(Domain)>::type;
+  using FRaw = typename std::decay<F>::type;
+
+  return PiecewiseConstantGridViewFunction<Range, GridView, FRaw>
+                                          (std::forward<F>(f), gridView);
 }
 
 }} // end namespace Dune::Functions
