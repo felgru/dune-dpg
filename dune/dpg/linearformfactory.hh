250807a09391f5ef6ed593e2c81a3edbadc0eccf
Felix Gruber <gruber@igpm.rwth-aachen.de>
2018-05-04 14:18:32 +0200

    add factory for linear forms

---
+++ dune/dpg/linearformfactory.hh
@@ -0,0 +1,147 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_DPG_LINEARFORMFACTORY_HH
+#define DUNE_DPG_LINEARFORMFACTORY_HH
+
+#include <tuple>
+#include <type_traits>
+
+#include <dune/dpg/assemble_types.hh>
+#include <dune/dpg/linearform.hh>
+#include <dune/dpg/linearfunctionalterm.hh>
+#include <dune/dpg/linearintegralterm.hh>
+#include <dune/dpg/spacetuple.hh>
+
+namespace Dune {
+  template<class TSpaces, class... LinearFormTerms>
+  class LinearFormFactory;
+
+  template<class TSpaces>
+  LinearFormFactory<TSpaces>
+  linearFormWithSpace(const TSpaces& testSpaces);
+
+  template<class TSpaces, class... LinearFormTerms>
+  class LinearFormFactory {
+    static_assert(is_SpaceTuplePtr<TSpaces>::value,
+        "TSpaces needs to be a SpaceTuplePtr!");
+    using TestSpacesPtr = TSpaces;
+    using TestSpaces = typename TestSpacesPtr::element_type;
+    using LinearFormTermsTuple = std::tuple<LinearFormTerms...>;
+
+    LinearFormFactory(const TestSpacesPtr& testSpaces,
+                        const LinearFormTermsTuple& terms)
+      : testSpaces(testSpaces)
+      , terms(terms)
+      {}
+
+    friend
+    LinearFormFactory<TSpaces>
+    linearFormWithSpace<TSpaces>(const TSpaces& testSpaces);
+
+    template<class TSpaces_, class... LinearFormTerms_>
+    friend class LinearFormFactory;
+
+    TestSpacesPtr testSpaces;
+    LinearFormTermsTuple terms;
+
+  public:
+    template<size_t spaceIndex,
+             LinearIntegrationType integrationType,
+             DomainOfIntegration domainOfIntegration,
+             class Factor>
+    auto addIntegralTerm(Factor c)
+    {
+      auto newTerm = make_LinearIntegralTerm<spaceIndex,
+                                             integrationType,
+                                             domainOfIntegration>(c);
+      using NewTerm = decltype(newTerm);
+      using NewLinearFormFactory
+        = LinearFormFactory<TSpaces, LinearFormTerms..., NewTerm>;
+      return NewLinearFormFactory{
+               testSpaces,
+               std::tuple_cat(terms, std::make_tuple(std::move(newTerm)))
+             };
+    }
+
+    template<size_t spaceIndex,
+             LinearIntegrationType integrationType,
+             DomainOfIntegration domainOfIntegration,
+             class Factor,
+             class Direction>
+    auto addIntegralTerm(Factor c, Direction beta)
+    {
+      auto newTerm = make_LinearIntegralTerm<spaceIndex,
+                                             integrationType,
+                                             domainOfIntegration>(c, beta);
+      using NewTerm = decltype(newTerm);
+      using NewLinearFormFactory
+        = LinearFormFactory<TSpaces, LinearFormTerms..., NewTerm>;
+      return NewLinearFormFactory{
+               testSpaces,
+               std::tuple_cat(terms, std::make_tuple(std::move(newTerm)))
+             };
+    }
+
+    template<size_t spaceIndex,
+             class SolutionSpace,
+             class FunctionalVector>
+    auto addFunctionalTerm(const FunctionalVector& functionalVector,
+                           const SolutionSpace& solutionSpace)
+    {
+      auto newTerm = make_LinearFunctionalTerm<spaceIndex, SolutionSpace,
+                                               FunctionalVector>
+                                              (functionalVector, solutionSpace);
+      using NewTerm = decltype(newTerm);
+      using NewLinearFormFactory
+        = LinearFormFactory<TSpaces, LinearFormTerms..., NewTerm>;
+      return NewLinearFormFactory{
+               testSpaces,
+               std::tuple_cat(terms, std::make_tuple(std::move(newTerm)))
+             };
+    }
+
+    template<size_t spaceIndex,
+             IntegrationType integrationType,
+             class SolutionSpace,
+             class FunctionalVector,
+             class Factor, class Direction,
+             typename std::enable_if<
+                         integrationType == IntegrationType::normalVector
+                      || integrationType ==
+                                  IntegrationType::travelDistanceWeighted
+                      >::type*
+               = nullptr
+            >
+    auto addSkeletalFunctionalTerm(
+        const FunctionalVector& functionalVector,
+        const SolutionSpace& solutionSpace,
+        Factor c, Direction beta)
+    {
+      auto newTerm = make_SkeletalLinearFunctionalTerm
+                      <spaceIndex, integrationType>
+                      (functionalVector, solutionSpace, c, beta);
+      using NewTerm = decltype(newTerm);
+      using NewLinearFormFactory
+        = LinearFormFactory<TSpaces, LinearFormTerms..., NewTerm>;
+      return NewLinearFormFactory{
+               testSpaces,
+               std::tuple_cat(terms, std::make_tuple(std::move(newTerm)))
+             };
+    }
+
+    LinearForm<TSpaces, std::tuple<LinearFormTerms...>>
+    create()
+    {
+      return {testSpaces, terms};
+    }
+  };
+
+  template<class TSpaces>
+  LinearFormFactory<TSpaces>
+  linearFormWithSpace(const TSpaces& testSpaces)
+  {
+    return {testSpaces, std::tuple<>{}};
+  }
+}
+
+#endif
