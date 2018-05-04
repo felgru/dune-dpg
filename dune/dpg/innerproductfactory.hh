dc9bb2b763b6a20bc61257320bba7a0cd659129c
Felix Gruber <gruber@igpm.rwth-aachen.de>
2018-05-04 12:21:49 +0200

    add factory for inner products

---
+++ dune/dpg/innerproductfactory.hh
@@ -0,0 +1,142 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_DPG_INNERPRODUCTFACTORY_HH
+#define DUNE_DPG_INNERPRODUCTFACTORY_HH
+
+#include <tuple>
+#include <type_traits>
+
+#include <dune/dpg/assemble_types.hh>
+#include <dune/dpg/innerproduct.hh>
+#include <dune/dpg/integralterm.hh>
+#include <dune/dpg/spacetuple.hh>
+
+namespace Dune {
+  template<class TSpaces, class... InnerProductTerms>
+  class InnerProductFactory;
+
+  template<class TSpaces>
+  InnerProductFactory<TSpaces>
+  innerProductWithSpace(const TSpaces& testSpaces);
+
+  template<class TSpaces, class... InnerProductTerms>
+  class InnerProductFactory {
+    static_assert(is_SpaceTuplePtr<TSpaces>::value,
+        "TSpaces needs to be a SpaceTuplePtr!");
+    using TestSpacesPtr = TSpaces;
+    using TestSpaces = typename TestSpacesPtr::element_type;
+    using InnerProductTermsTuple = std::tuple<InnerProductTerms...>;
+
+    InnerProductFactory(const TestSpacesPtr& testSpaces,
+                        const InnerProductTermsTuple& terms)
+      : testSpaces(testSpaces)
+      , terms(terms)
+      {}
+
+    friend
+    InnerProductFactory<TSpaces>
+    innerProductWithSpace<TSpaces>(const TSpaces& testSpaces);
+
+    template<class TSpaces_, class... InnerProductTerms_>
+    friend class InnerProductFactory;
+
+    TestSpacesPtr testSpaces;
+    InnerProductTermsTuple terms;
+
+  public:
+    template<size_t lhsSpaceIndex,
+             size_t rhsSpaceIndex,
+             IntegrationType integrationType,
+             DomainOfIntegration domainOfIntegration,
+             class Factor,
+             typename std::enable_if<
+                         integrationType == IntegrationType::valueValue
+                      || integrationType == IntegrationType::normalSign>::type*
+                    = nullptr
+            >
+    auto addIntegralTerm(Factor c)
+    {
+      auto newTerm = make_IntegralTerm<lhsSpaceIndex,
+                                       rhsSpaceIndex,
+                                       integrationType,
+                                       domainOfIntegration>(c);
+      using NewTerm = decltype(newTerm);
+      using NewInnerProductFactory
+        = InnerProductFactory<TSpaces, InnerProductTerms..., NewTerm>;
+      return NewInnerProductFactory{
+               testSpaces,
+               std::tuple_cat(terms, std::make_tuple(std::move(newTerm)))
+             };
+    }
+
+    template<size_t lhsSpaceIndex,
+             size_t rhsSpaceIndex,
+             IntegrationType integrationType,
+             DomainOfIntegration domainOfIntegration,
+             class Factor, class Direction,
+             typename std::enable_if<
+                         integrationType == IntegrationType::gradValue
+                      || integrationType == IntegrationType::valueGrad
+                      || integrationType == IntegrationType::gradGrad
+                      || integrationType == IntegrationType::normalVector
+                      || integrationType ==
+                                IntegrationType::travelDistanceWeighted
+                      >::type*
+               = nullptr
+            >
+    auto addIntegralTerm(Factor c, Direction beta)
+    {
+      auto newTerm = make_IntegralTerm<lhsSpaceIndex,
+                                       rhsSpaceIndex,
+                                       integrationType,
+                                       domainOfIntegration>(c, beta);
+      using NewTerm = decltype(newTerm);
+      using NewInnerProductFactory
+        = InnerProductFactory<TSpaces, InnerProductTerms..., NewTerm>;
+      return NewInnerProductFactory{
+               testSpaces,
+               std::tuple_cat(terms, std::make_tuple(std::move(newTerm)))
+             };
+    }
+
+    template<size_t lhsSpaceIndex,
+             size_t rhsSpaceIndex,
+             IntegrationType integrationType,
+             DomainOfIntegration domainOfIntegration,
+             class Factor, class Direction,
+             typename std::enable_if<
+                         integrationType == IntegrationType::gradGrad>::type*
+                    = nullptr
+            >
+    auto addIntegralTerm(Factor c, Direction lhsBeta, Direction rhsBeta)
+    {
+      auto newTerm = make_IntegralTerm<lhsSpaceIndex,
+                                       rhsSpaceIndex,
+                                       integrationType,
+                                       domainOfIntegration>
+                                      (c, lhsBeta, rhsBeta);
+      using NewTerm = decltype(newTerm);
+      using NewInnerProductFactory
+        = InnerProductFactory<TSpaces, InnerProductTerms..., NewTerm>;
+      return NewInnerProductFactory{
+               testSpaces,
+               std::tuple_cat(terms, std::make_tuple(std::move(newTerm)))
+             };
+    }
+
+    InnerProduct<TSpaces, std::tuple<InnerProductTerms...>>
+    create()
+    {
+      return {testSpaces, terms};
+    }
+  };
+
+  template<class TSpaces>
+  InnerProductFactory<TSpaces>
+  innerProductWithSpace(const TSpaces& testSpaces)
+  {
+    return {testSpaces, std::tuple<>{}};
+  }
+}
+
+#endif
