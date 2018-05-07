77cb9c64fee0ec216ccf3fad54cac856a00e2001
Felix Gruber <gruber@igpm.rwth-aachen.de>
2018-05-07 11:42:04 +0200

    [bugfix] fix compilation with GCC 6
    Inline variables are a C++ 17 feature that GCC 6 cannot handle.
    Removing the inline solves the problem.

--- dune/dpg/type_traits.hh
+++ dune/dpg/type_traits.hh
@@ -440,7 +440,7 @@ struct uses_only_constant_coefficients<std::tuple<Terms...>>
       uses_only_constant_coefficients<typename Terms::Term>...> {};
 
 template<class Term>
-inline constexpr bool uses_only_constant_coefficients_v
+constexpr bool uses_only_constant_coefficients_v
     = uses_only_constant_coefficients<Term>::value;
 
 } // end namespace Dune
