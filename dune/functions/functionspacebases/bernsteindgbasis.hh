felgru/dune-dpg

commit fe4c596b5956f6f46c1325539790664cc366eb41

Implement indices method on all our PreBasis classes
This method was introduces in Dune 2.8 to replace the now deprecated
NodeIndexSet classes.

For now I've only added the indices method to normal dune-functions-like
classes and not our constrained basis classes or the DG-refined or
subsampled basis classes.

dune/functions/functionspacebases/bernsteindgbasis.hh
@@ -178,6 +178,55 @@ public:
     return StaticPower<(k+1),GV::dimension>::power;
   }
 
+  //! Maps from subtree index set [0..size-1] to a globally unique multi index in global basis
+  template<typename It>
+  It indices(const Node& node, It it) const
+  {
+    const auto& gridIndexSet = gridView().indexSet();
+    const auto& element = node.element();
+
+    for (size_type i = 0, end = size(); i < end; ++i, ++it) {
+      if constexpr (dim == 1) {
+        *it = {dofsPerEdge*gridIndexSet.subIndex(element,0,0) + i};
+        continue;
+      } else if constexpr (dim == 2) {
+        if (element.type().isTriangle()) {
+          *it = {dofsPerTriangle*gridIndexSet.subIndex(element,0,0) + i};
+          continue;
+        } else if (element.type().isQuadrilateral()) {
+          *it = {quadrilateralOffset_
+                 + dofsPerQuad*gridIndexSet.subIndex(element,0,0) + i};
+          continue;
+        } else {
+          DUNE_THROW(Dune::NotImplemented, "2d elements have to be triangles or quadrilaterals");
+        }
+      } else if constexpr (dim == 3) {
+        if (element.type().isTetrahedron()) {
+          *it = {dofsPerTetrahedron*gridIndexSet.subIndex(element,0,0) + i};
+          continue;
+        } else if (element.type().isPrism()) {
+          *it = {prismOffset_
+                 + dofsPerPrism*gridIndexSet.subIndex(element,0,0) + i};
+          continue;
+        } else if (element.type().isHexahedron()) {
+          *it = {hexahedronOffset_
+                 + dofsPerHexahedron*gridIndexSet.subIndex(element,0,0) + i};
+          continue;
+        } else if (element.type().isPyramid()) {
+          *it = {pyramidOffset_
+                 + dofsPerPyramid*gridIndexSet.subIndex(element,0,0) + i};
+          continue;
+        } else {
+          DUNE_THROW(Dune::NotImplemented, "3d elements have to be tetrahedrons, prisms, hexahedrons or pyramids");
+        }
+      } else {
+        static_assert(dim >= 1 && dim <= 3,
+                      "The index method is not yet implemented for grids of this dimension!");
+      }
+    }
+    return it;
+  }
+
 //protected:
   GridView gridView_;
 
@@ -214,7 +263,8 @@ public:
 #endif
 
   BernsteinDGNodeIndexSet(const PreBasis& preBasis) :
-    preBasis_(&preBasis)
+    preBasis_(&preBasis),
+    node_(nullptr)
   {}
 
   /** \brief Bind the view to a grid element
@@ -245,45 +295,8 @@ public:
   template<typename It>
   It indices(It it) const
   {
-    const auto& gridIndexSet = preBasis_->gridView().indexSet();
-    const auto& element = node_->element();
-
-    for (size_type i = 0, end = size(); i < end; ++i, ++it) {
-      if constexpr (dim == 1) {
-        *it = {preBasis_->dofsPerEdge*gridIndexSet.subIndex(element,0,0) + i};
-        continue;
-      } else if constexpr (dim == 2) {
-        if (element.type().isTriangle()) {
-          *it = {preBasis_->dofsPerTriangle*gridIndexSet.subIndex(element,0,0) + i};
-          continue;
-        } else if (element.type().isQuadrilateral()) {
-          *it = {preBasis_->quadrilateralOffset_ + preBasis_->dofsPerQuad*gridIndexSet.subIndex(element,0,0) + i};
-          continue;
-        } else {
-          DUNE_THROW(Dune::NotImplemented, "2d elements have to be triangles or quadrilaterals");
-        }
-      } else if constexpr (dim == 3) {
-        if (element.type().isTetrahedron()) {
-          *it = {preBasis_->dofsPerTetrahedron*gridIndexSet.subIndex(element,0,0) + i};
-          continue;
-        } else if (element.type().isPrism()) {
-          *it = { preBasis_->prismOffset_ + preBasis_->dofsPerPrism*gridIndexSet.subIndex(element,0,0) + i};
-          continue;
-        } else if (element.type().isHexahedron()) {
-          *it = { preBasis_->hexahedronOffset_ + preBasis_->dofsPerHexahedron*gridIndexSet.subIndex(element,0,0) + i};
-          continue;
-        } else if (element.type().isPyramid()) {
-          *it = { preBasis_->pyramidOffset_ + preBasis_->dofsPerPyramid*gridIndexSet.subIndex(element,0,0) + i};
-          continue;
-        } else {
-          DUNE_THROW(Dune::NotImplemented, "3d elements have to be tetrahedrons, prisms, hexahedrons or pyramids");
-        }
-      } else {
-        static_assert(dim >= 1 && dim <= 3,
-                      "The index method is not yet implemented for grids of this dimension!");
-      }
-    }
-    return it;
+    assert(node_ != nullptr);
+    return preBasis_->indices(*node_, it);
   }
 
 protected:

dune/functions/functionspacebases/lagrangefacebasis.hh
@@ -179,6 +179,70 @@ public:
     return 4*(k+1);
   }
 
+  //! Maps from subtree index set [0..size-1] to a globally unique multi index in global basis
+  template<typename It>
+  It indices(const Node& node, It it) const
+  {
+    const auto& gridIndexSet = gridView().indexSet();
+    const auto& element = node.element();
+
+    for (size_type i = 0, end = this->size(); i < end; ++it, ++i)
+    {
+      const Dune::LocalKey localKey
+          = node.finiteElement().localCoefficients().localKey(i);
+      // The dimension of the entity that the current dof is related to
+      const size_t dofDim = dim - localKey.codim();
+      if (dofDim==0) {  // vertex dof
+        *it = {{ gridIndexSet.subIndex(element,localKey.subEntity(),dim) }};
+        continue;
+      }
+
+      if (dofDim==1)
+      {  // edge dof
+        if constexpr (dim==1)   // element dof -- any local numbering is fine
+        {
+          DUNE_THROW(Dune::NotImplemented,
+              "faces have no elements of codimension 0");
+        }
+        else
+        {
+          const Dune::ReferenceElement<double,dim> refElement
+              = Dune::referenceElement<double,dim>(element.type());
+
+          // we have to reverse the numbering if the local triangle edge is
+          // not aligned with the global edge
+          size_t v0 = gridIndexSet.subIndex(element,refElement.subEntity(localKey.subEntity(),localKey.codim(),0,dim),dim);
+          size_t v1 = gridIndexSet.subIndex(element,refElement.subEntity(localKey.subEntity(),localKey.codim(),1,dim),dim);
+          bool flip = (v0 > v1);
+          *it = {{ (flip)
+                   ? edgeOffset_
+                     + (k+1)*gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim())
+                     + k-localKey.index()
+                   : edgeOffset_
+                     + (k+1)*gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim())
+                     + localKey.index() }};
+          continue;
+        }
+      }
+
+      if (dofDim==2)
+      {
+        if constexpr (dim==2)   // element dof -- any local numbering is fine
+        {
+          DUNE_THROW(Dune::NotImplemented,
+                     "faces have no elements of codimension 0");
+        } else
+        {
+          DUNE_THROW(Dune::NotImplemented,
+                     "LagrangeFaceNodalBasis for 3D grids is not implemented");
+        }
+      }
+      DUNE_THROW(Dune::NotImplemented,
+          "Grid contains elements not supported for the LagrangeFaceNodalBasis");
+    }
+    return it;
+  }
+
 //protected:
   GridView gridView_;
 
@@ -284,7 +348,8 @@ public:
 #endif
 
   LagrangeFaceNodeIndexSet(const PreBasis& preBasis) :
-    preBasis_(&preBasis)
+    preBasis_(&preBasis),
+    node_(nullptr)
   {}
 
   /** \brief Bind the view to a grid element
@@ -317,64 +382,7 @@ public:
   It indices(It it) const
   {
     assert(node_ != nullptr);
-    const auto& gridIndexSet = preBasis_->gridView().indexSet();
-    const auto& element = node_->element();
-
-    for (size_type i = 0, end = this->size(); i < end; ++it, ++i)
-    {
-      const Dune::LocalKey localKey
-          = node_->finiteElement().localCoefficients().localKey(i);
-      // The dimension of the entity that the current dof is related to
-      const size_t dofDim = dim - localKey.codim();
-      if (dofDim==0) {  // vertex dof
-        *it = {{ gridIndexSet.subIndex(element,localKey.subEntity(),dim) }};
-        continue;
-      }
-
-      if (dofDim==1)
-      {  // edge dof
-        if constexpr (dim==1)   // element dof -- any local numbering is fine
-        {
-          DUNE_THROW(Dune::NotImplemented,
-              "faces have no elements of codimension 0");
-        }
-        else
-        {
-          const Dune::ReferenceElement<double,dim> refElement
-              = Dune::referenceElement<double,dim>(element.type());
-
-          // we have to reverse the numbering if the local triangle edge is
-          // not aligned with the global edge
-          size_t v0 = gridIndexSet.subIndex(element,refElement.subEntity(localKey.subEntity(),localKey.codim(),0,dim),dim);
-          size_t v1 = gridIndexSet.subIndex(element,refElement.subEntity(localKey.subEntity(),localKey.codim(),1,dim),dim);
-          bool flip = (v0 > v1);
-          *it = {{ (flip)
-                   ? preBasis_->edgeOffset_
-                     + (k+1)*gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim())
-                     + k-localKey.index()
-                   : preBasis_->edgeOffset_
-                     + (k+1)*gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim())
-                     + localKey.index() }};
-          continue;
-        }
-      }
-
-      if (dofDim==2)
-      {
-        if constexpr (dim==2)   // element dof -- any local numbering is fine
-        {
-          DUNE_THROW(Dune::NotImplemented,
-                     "faces have no elements of codimension 0");
-        } else
-        {
-          DUNE_THROW(Dune::NotImplemented,
-                     "LagrangeFaceNodalBasis for 3D grids is not implemented");
-        }
-      }
-      DUNE_THROW(Dune::NotImplemented,
-          "Grid contains elements not supported for the LagrangeFaceNodalBasis");
-    }
-    return it;
+    return preBasis_->indices(*node_, it);
   }
 
 protected:

dune/functions/functionspacebases/lagrangetracebasis.hh
@@ -180,6 +180,78 @@ public:
            - StaticPower<(k-1),GV::dimension>::power;
   }
 
+  //! Maps from subtree index set [0..size-1] to a globally unique multi index in global basis
+  template<typename It>
+  It indices(const Node& node, It it) const
+  {
+    const auto& gridIndexSet = gridView().indexSet();
+    const auto& element = node.element();
+
+    for (size_type i = 0, end = this->size(); i < end; ++it, ++i)
+    {
+      const Dune::LocalKey localKey
+          = node.finiteElement().localCoefficients().localKey(i);
+      // The dimension of the entity that the current dof is related to
+      const size_t dofDim = dim - localKey.codim();
+      if (dofDim==0) {  // vertex dof
+        *it = {{ gridIndexSet.subIndex(element,localKey.subEntity(),dim) }};
+        continue;
+      }
+
+      if (dofDim==1)
+      {  // edge dof
+        if constexpr (dim==1)   // element dof -- any local numbering is fine
+        {
+          DUNE_THROW(Dune::NotImplemented,
+              "traces have no elements of codimension 0");
+        }
+        else
+        {
+          const auto refElement
+              = Dune::referenceElement<double,dim>(element.type());
+
+          // we have to reverse the numbering if the local triangle edge is
+          // not aligned with the global edge
+          size_t v0 = gridIndexSet.subIndex(element,refElement.subEntity(localKey.subEntity(),localKey.codim(),0,dim),dim);
+          size_t v1 = gridIndexSet.subIndex(element,refElement.subEntity(localKey.subEntity(),localKey.codim(),1,dim),dim);
+          bool flip = (v0 > v1);
+          *it = {{ (flip)
+                   ? edgeOffset_
+                     + (k-1)*gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim())
+                     + (k-2)-localKey.index()
+                   : edgeOffset_
+                     + (k-1)*gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim())
+                     + localKey.index() }};
+          continue;
+        }
+      }
+
+      if (dofDim==2)
+      {
+        if constexpr (dim==2)   // element dof -- any local numbering is fine
+        {
+          DUNE_THROW(Dune::NotImplemented,
+              "traces have no elements of codimension 0");
+        } else
+        {
+          const auto refElement
+              = Dune::referenceElement<double,dim>(element.type());
+
+          if (! refElement.type(localKey.subEntity(), localKey.codim()).isTriangle()
+              or k>3)
+            DUNE_THROW(Dune::NotImplemented, "LagrangeTraceNodalBasis for 3D grids is only implemented if k<=3 and if the grid is a simplex grid");
+
+          *it = {{ triangleOffset_
+                   + gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim()) }};
+          continue;
+        }
+      }
+      DUNE_THROW(Dune::NotImplemented,
+          "Grid contains elements not supported for the LagrangeTraceNodalBasis");
+    }
+    return it;
+  }
+
 //protected:
   GridView gridView_;
 
@@ -286,7 +358,8 @@ public:
 #endif
 
   LagrangeTraceNodeIndexSet(const PreBasis& preBasis) :
-    preBasis_(&preBasis)
+    preBasis_(&preBasis),
+    node_(nullptr)
   {}
 
   /** \brief Bind the view to a grid element
@@ -319,72 +392,7 @@ public:
   It indices(It it) const
   {
     assert(node_ != nullptr);
-    const auto& gridIndexSet = preBasis_->gridView().indexSet();
-    const auto& element = node_->element();
-
-    for (size_type i = 0, end = this->size(); i < end; ++it, ++i)
-    {
-      const Dune::LocalKey localKey
-          = node_->finiteElement().localCoefficients().localKey(i);
-      // The dimension of the entity that the current dof is related to
-      const size_t dofDim = dim - localKey.codim();
-      if (dofDim==0) {  // vertex dof
-        *it = {{ gridIndexSet.subIndex(element,localKey.subEntity(),dim) }};
-        continue;
-      }
-
-      if (dofDim==1)
-      {  // edge dof
-        if constexpr (dim==1)   // element dof -- any local numbering is fine
-        {
-          DUNE_THROW(Dune::NotImplemented,
-              "traces have no elements of codimension 0");
-        }
-        else
-        {
-          const auto refElement
-              = Dune::referenceElement<double,dim>(element.type());
-
-          // we have to reverse the numbering if the local triangle edge is
-          // not aligned with the global edge
-          size_t v0 = gridIndexSet.subIndex(element,refElement.subEntity(localKey.subEntity(),localKey.codim(),0,dim),dim);
-          size_t v1 = gridIndexSet.subIndex(element,refElement.subEntity(localKey.subEntity(),localKey.codim(),1,dim),dim);
-          bool flip = (v0 > v1);
-          *it = {{ (flip)
-                   ? preBasis_->edgeOffset_
-                     + (k-1)*gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim())
-                     + (k-2)-localKey.index()
-                   : preBasis_->edgeOffset_
-                     + (k-1)*gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim())
-                     + localKey.index() }};
-          continue;
-        }
-      }
-
-      if (dofDim==2)
-      {
-        if constexpr (dim==2)   // element dof -- any local numbering is fine
-        {
-          DUNE_THROW(Dune::NotImplemented,
-              "traces have no elements of codimension 0");
-        } else
-        {
-          const auto refElement
-              = Dune::referenceElement<double,dim>(element.type());
-
-          if (! refElement.type(localKey.subEntity(), localKey.codim()).isTriangle()
-              or k>3)
-            DUNE_THROW(Dune::NotImplemented, "LagrangeTraceNodalBasis for 3D grids is only implemented if k<=3 and if the grid is a simplex grid");
-
-          *it = {{ preBasis_->triangleOffset_
-                   + gridIndexSet.subIndex(element,localKey.subEntity(),localKey.codim()) }};
-          continue;
-        }
-      }
-      DUNE_THROW(Dune::NotImplemented,
-          "Grid contains elements not supported for the LagrangeTraceNodalBasis");
-    }
-    return it;
+    return preBasis_->indices(*node_, it);
   }
 
 protected:

commit 915f8771adec1c6ad68746be5d4fbf33de12a465

Fix indices methods
I forgot to change the call of the size method when moving the indices
code to the PreBasis.

This fixes a bug introduced with
fe4c596b5956f6f46c1325539790664cc366eb41.

dune/functions/functionspacebases/bernsteindgbasis.hh
@@ -185,7 +185,7 @@ public:
     const auto& gridIndexSet = gridView().indexSet();
     const auto& element = node.element();
 
-    for (size_type i = 0, end = size(); i < end; ++i, ++it) {
+    for (size_type i = 0, end = node.finiteElement().size(); i < end; ++i, ++it) {
       if constexpr (dim == 1) {
         *it = {dofsPerEdge*gridIndexSet.subIndex(element,0,0) + i};
         continue;

dune/functions/functionspacebases/lagrangefacebasis.hh
@@ -185,11 +185,12 @@ public:
   {
     const auto& gridIndexSet = gridView().indexSet();
     const auto& element = node.element();
+    const auto& finiteElement = node.finiteElement();
 
-    for (size_type i = 0, end = this->size(); i < end; ++it, ++i)
+    for (size_type i = 0, end = finiteElement.size(); i < end; ++it, ++i)
     {
       const Dune::LocalKey localKey
-          = node.finiteElement().localCoefficients().localKey(i);
+          = finiteElement.localCoefficients().localKey(i);
       // The dimension of the entity that the current dof is related to
       const size_t dofDim = dim - localKey.codim();
       if (dofDim==0) {  // vertex dof

dune/functions/functionspacebases/lagrangetracebasis.hh
@@ -186,11 +186,12 @@ public:
   {
     const auto& gridIndexSet = gridView().indexSet();
     const auto& element = node.element();
+    const auto& finiteElement = node.finiteElement();
 
-    for (size_type i = 0, end = this->size(); i < end; ++it, ++i)
+    for (size_type i = 0, end = finiteElement.size(); i < end; ++it, ++i)
     {
       const Dune::LocalKey localKey
-          = node.finiteElement().localCoefficients().localKey(i);
+          = finiteElement.localCoefficients().localKey(i);
       // The dimension of the entity that the current dof is related to
       const size_t dofDim = dim - localKey.codim();
       if (dofDim==0) {  // vertex dof